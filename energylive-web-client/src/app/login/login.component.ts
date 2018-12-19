5f6f421d883bd6396c317093d1d2249097c1f8ed
rasinrajk <rasinrajk@gmail.com>
2018-12-19 23:25:48 +0530

    view modified

--- energylive-web-client/src/app/login/login.component.ts
+++ energylive-web-client/src/app/login/login.component.ts
@@ -1,4 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { SawtoothService } from '../sawtooth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  template: `
+  <div class="count">
+  <div class="form">  
+  <form >
+ 
+  <input type="file" id="files" name="files[]" multiple (change)="onChange($event)"  />
+   <output id="list"></output>
+   </form>
+   </div>
+ </div>
+ 
+  `,
+  styles: [
+    "../node_modules/angular2-busy/build/style/busy.css",
+    "styles.css"
+  ]
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router : Router ,private Data : SawtoothService) { 
+    this.Data.clearLogin();
+  }
+
+  ngOnInit() {
+    this.Data.clearLogin();
+    //this.router.navigate(['login/home']);
+  } 
+
+  
+onChange(event) {
+  event.preventDefault()
+  console.log(event);
+    var file = event.target.files[0];
+    console.log("file",file);
+    var reader = new FileReader();
+    const dataService = this.Data;
+    const router = this.router;
+    reader.onload = function(event) {
+      console.log("target",event.target);
+      const result=(reader.result);
+        console.log("target result",event.target);
+        const login = dataService.setLogin(file.name,result);
+        if(login!=false) {
+          router.navigate(['login/home']);
+        }
+
+    };
+    
+    reader.onerror = (e) => {
+      console.log(e);
+     
+    };
+    reader.readAsArrayBuffer(file);
+    
+  }
+    
+ }
+    
+ 
+
+
+/*import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
@@ -51,4 +118,4 @@ export class LoginComponent implements OnInit {
             this.router.navigate(['home']);
         });
     }
-}
+}*/
